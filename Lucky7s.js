f5038694faeda83092266071955ebe84bb0a3eef
kkord <elven.banzon@gmail.com>
2018-09-18 14:12:14 -0500

    more functions

--- Lucky7s.js
+++ Lucky7s.js
@@ -11,11 +11,22 @@ Hint: For steps 4, 5, and 6, declare some variables.
 When the game is over, display the following output:
 
 */
-var dice1 = 0;
-var dice2 = 0;
-var rollCount = 0;
+var startingBet = document.getElementById("startingBet").value;
+var dice1=0;
+var dice2=0;
+var diceTotal=0;
+var rollCount=0;
 
-function rollDice {
+function validateItems(){
+  clearErrors();
+  var bet = document.forms["startingBet"].value;
+  if (bet == "" || isNaN(bet) || bet == 0) {
+    alert("Your starting bet must be a number greater than 0.");
+    document.forms["startingBet"].focus();
+    return false;
+  }
+
+function rollDice() {
   rollCount++;
   dice1=Math.floor(Math.random()*6)+1;
   dice2=Math.floor(Math.random()*6)+1;
@@ -25,8 +36,20 @@ function rollDice {
 
 function resetForm() {
   clearErrors();
-  document.forms["numberFun"]["num1"].value="";
+  document.forms["startingBet"].value="";
   document.getElementById("results").style.display="none";
   document.getElementById("submitButton").innerText="Submit";
   document.forms["numberFun"]["num1"].focus();
+}
+
+
+function displayResults() {
+  document.getElementById("results").style.display="block";
+  document.getElementById("submitButton").style.display="Recalculate";
+  document.getElementById("addResult").innerText = Number(num1) + Number(num2);
+  document.getElementById("subtractResult").innerText = num1 - num2;
+  document.getElementById("multiplyResult").innerText = num1 * num2;
+  document.getElementById("divideResult").innerText = num1 / num2;
+  // return false so the form doesn't submit and can see the results
+  return false;
 }
